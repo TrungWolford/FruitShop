a3d1d2e5594df55285ceedae7088da7185005aea
VuNguyen <187271812+CodeWithVu@users.noreply.github.com>
2025-10-07 11:12:56 +0700

    add config file

--- FruitShop-UI/eslint.config.js
+++ FruitShop-UI/eslint.config.js
@@ -23,7 +23,43 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
+       //React rules
+       'react-refresh/only-export-components':'warn',
+       'react-hooks/rules-of-hooks': 'error',
+       'react-hooks/exhaustive-deps': 'warn',
+       'react/prop-types': 'off',
+       'react/display-name': 'off',
+       'react/jsx-uses-react': 'error',
+       'react/jsx-uses-vars': 'error',
+   
+       //MUI rules
+       'no-restricted-imports': [
+         'error',
+         {
+           'patterns': ['@mui/*/*/*']
+         }
+       ],
+   
+       //Common rules
+       'no-console': 'warn',
+       'no-lonely-if': 'warn',
+       'no-unused-vars': 'warn',
+       'no-trailing-spaces': 'warn',
+       'no-multi-spaces': 'warn',
+       'no-multiple-empty-lines': 'warn',
+       'space-before-blocks': ['error', 'always'],
+       'object-curly-spacing': ['warn', 'always'],
+       'indent': ['warn', 2],
+       'semi': ['warn', 'never'],
+       'quotes': ['error', 'single'],
+       'array-bracket-spacing': 'warn',
+       'linebreak-style': 'off',
+       'no-unexpected-multiline': 'warn',
+       'keyword-spacing': 'warn',
+       'comma-dangle': 'warn',
+       'comma-spacing': 'warn',
+       'arrow-spacing': 'warn',
+       'no-undef': 'error'
+     },
   },
 ])
