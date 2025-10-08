7303f3ac0e4e19c4a924f876e2de153d1b20fb8a
T Wolford <121479182+TrungWolford@users.noreply.github.com>
2025-10-08 10:01:36 +0700

    BE: Create request + response Role

---
+++ server/src/main/java/server/FruitShop/dto/response/Role/RoleResponse.java
@@ -0,0 +1,18 @@
+package server.FruitShop.dto.response.Role;
+
+import lombok.Data;
+import server.FruitShop.entity.Role;
+
+@Data
+public class RoleResponse {
+    private String roleId;
+    private String roleName;
+
+    public static RoleResponse fromEntity(Role role){
+        RoleResponse response = new RoleResponse();
+        response.setRoleId(role.getRoleId());
+        response.setRoleName(response.getRoleName());
+
+        return response;
+    }
+}
