dfbdbc5c00143c2600166ddced06dc4315aa542b
T Wolford <121479182+TrungWolford@users.noreply.github.com>
2025-10-09 07:20:17 +0700

    BE: Create Product + ProductImage entity

---
+++ server/src/main/java/server/FruitShop/entity/ProductImage.java
@@ -0,0 +1,24 @@
+package server.FruitShop.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "product_images")
+@Data
+public class ProductImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "image_order")
+    private Integer imageOrder;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
